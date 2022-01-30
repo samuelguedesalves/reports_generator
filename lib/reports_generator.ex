41693dfbeeb4a7c0da44c0c50f0d8f2442bb2fbf
Samuel Guedes <guedes.works7@gmail.com>
2022-01-30 13:46:52 -0400

    feat: learn read file and pipe operator with pathern match

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,10 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(filename) do
+    "./reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  def handle_file({:ok, file_content}), do: file_content
+  def handle_file({:error, _reason}), do: "Error while opening file!"
 end
